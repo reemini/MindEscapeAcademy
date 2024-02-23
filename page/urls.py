7b5e242650181f3105f3d5058e538e51270c4592
Fatim99 <87813480+fatim99@users.noreply.github.com>
2024-02-23 20:28:40 +0300

    This is done by fatima

---
+++ page/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path ('signup',views.signup, name = 'signup'),
+    path ('login',views.login, name = 'login'),
+    path ('courseinfo',views.courseinfo, name = 'courseinfo'),
+    path ('header',views.header, name = 'header'),
+
+
+
+]
